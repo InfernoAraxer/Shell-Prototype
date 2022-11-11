9982485688f7b45e41d45eaab00d0d90bb8a8fc4
Alex Tan <at1207@ilab2.cs.rutgers.edu>
2022-11-11 11:53:21 -0500

    Added Jobs, without background, and stated cd

--- shell.c
+++ shell.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <unistd.h>
 #include <signal.h>
 #include <errno.h>
 #include <unistd.h>
@@ -19,6 +19,7 @@ struct job{
 	pid_t pid;
 	int status; //1 for Running, 2 for Stopped, 3 for Terminated
 	char** command;
+    int commandLength;
 	job* next;
 };
 
@@ -60,24 +61,32 @@ char** readInput(int* length) {
     char c;
     while(1) {
         int j = 2;
+        args = realloc(args, (i+1) * sizeof(char *));
         args[i] = malloc(j * sizeof(char));
         scanf("%c", &c);
-        if (c == '\n') {
+        if (feof(stdin)) {
+            *length = 0;
+            for (int k = 0; k < i + 1; k++) {
+                free(args[i]);
+            }
+            return args;
+        }
+        if (c == '\n' && !feof(stdin)) {
             free(args[i]);
             args[i] = NULL;
             *length = i;
             return args;
         }
-        while (c == ' ') {
+        while (c == ' ' && !feof(stdin)) {
             scanf("%c", &c);
         }
-        if (c == '\n') {
+        if (c == '\n' && !feof(stdin)) {
             free(args[i]);
             args[i] = NULL;
             *length = i;
             return args;
         }
-        while(c != ' ' && c != '\n') {
+        while(c != ' ' && c != '\n' && !feof(stdin)) {
             args[i][j-2] = c;
             j++;
             args[i] = realloc(args[i], j * sizeof(char));
@@ -85,13 +94,11 @@ char** readInput(int* length) {
         }
         args[i][j-2] = '\0';
         i++;
-        args = realloc(args, (i+1) * sizeof(char *));
-        if (c == '\n') {
+        if (c == '\n' && !feof(stdin)) {
             if (j == 2) {
                 free(args[0]);
                 i-=1;
             }
-            args[i] = NULL;
             *length = i;
             return args;
         }
@@ -132,13 +139,17 @@ int main() {
     jobList->status = 0;
     char* home = getenv("HOME");
     setenv("PWD", home, 1);
+    int length = 0;
+    int status = 0;
+    int bg = 0;
+    char** args;
     while(!feof(stdin)) {
         printf("> ");
-        int length = 0;
-        int status = 0;
-        int bg = 0;
+        length = 0;
+        status = 0;
+        bg = 0;
         pid_t tempPid;
-        char** args;
+        args = NULL;
         char* pwd = getenv("PWD");
 
         //read input
@@ -153,165 +164,192 @@ int main() {
         }
 
         // Determines if Process will run in the background
-        bg = background(args, length);
+        if (!feof(stdin)) {
+            bg = background(args, length);
+            if (!strcmp(args[0], "bg")) {
+                // Input bg <jobID>: Run a suspended job in the background
 
-        if (!strcmp(args[0], "bg")) {
-            // Input bg <jobID>: Run a suspended job in the background
+                free(args[0]);
+                free(args);
+            } else if (!strcmp(args[0], "cd")) {
+                // cd [path]: Changes current directory to the given absolute or relative path. 
+                //           If no path is give, use the value of environment variable HOME.
+                //           Your shell should update the environment variable PWD with the
+                //           (absolute) present working directory after running cd.
 
-        } else if (!strcmp(args[0], "cd")) {
-            // cd [path]: Changes current directory to the given absolute or relative path. 
-            //           If no path is give, use the value of environment variable HOME.
-            //           Your shell should update the environment variable PWD with the
-            //           (absolute) present working directory after running cd.
+                
+                if (length == 1) {
+                    setenv("PWD", home, 1);
+                } else {
+                    char* path = malloc((strlen(pwd) + strlen(args[1]) + 11)*sizeof(char));
+                    int exists = -1;
+                    struct stat buf;
+                    strcpy(path, pwd);
+                    if (args[0][0] != '/') {
+                        strcat(path, "/");
+                    }
+                    for (int i = 1; i < length; i++) {
+                        strcat(path, args[i]);
+                    }
+                    printf("%s   ", path); // Testing Line
 
-        } else if (!strcmp(args[0], "exit")) {
-            // exit: Exit the shell. The shell should also exit if the user hits ctrl-d on
-            //       on an empty input line. When the shell exist, it should first send SIGHUP
-            //       followed by SIGCONT to any stopped jobs, and SIGHUP to any running jobs.
-            return 0;
-        } else if (!strcmp(args[0], "fg")) {
-            // fg <jobID>L Run a suspended or background job in the foreground
+                    exists = stat(path, &buf);
 
-        } else if (!strcmp(args[0], "jobs")) {
-            // jobs: List current jobs, including their jobID, processID, current status, and
-            //       command. If no jobs exist, this should print nothing.
+                    if (!exists && S_ISDIR(buf.st_mode)) {
+                        setenv("PWD", path, 1);
+                    } else if (!exists) {
+                        printf("bash: cd: %s: Not a directory\n", args[1]);
+                    } else {
+                        printf("bash: cd: %s: No such file or directory\n", args[1]);
+                    }
+                    free(path);
+                }
 
-            fflush(stdout);
+                
+                for (int i = 0; i < length; i++) {
+                    free(args[i]);
+                }                
+                free(args);
+            } else if (!strcmp(args[0], "exit")) {
+                // exit: Exit the shell. The shell should also exit if the user hits ctrl-d on
+                //       on an empty input line. When the shell exist, it should first send SIGHUP
+                //       followed by SIGCONT to any stopped jobs, and SIGHUP to any running jobs.
+                
+                return 0;
+            } else if (!strcmp(args[0], "fg")) {
+                // fg <jobID>L Run a suspended or background job in the foreground
 
-            // printf("I am the child.\n");
-            // printf("The child is about to read from the pipe.\n");
-            // while (read(pipefd[0], &buf, 1) > 0){
-            //     
-            // }
-            // write(STDOUT_FILENO, "\n\n", 2);
+                free(args[0]);
+                free(args);
+            } else if (!strcmp(args[0], "jobs")) {
+                // jobs: List current jobs, including their jobID, processID, current status, and
+                //       command. If no jobs exist, this should print nothing.
+                
+                if (jobList->status != 0) {
+                    job* temp = jobList; 
+                    int i = 1;
+                    while (temp->status != 0) {
+                        printf("[%d] %d %s %s", i, temp->pid, getStatus(temp->status), temp->command[0]);
+                        for (int i = 1; i < temp->commandLength; i++) {
+                            printf("%s ", temp->command[i]);
+                        }
+                        printf("\n");
+                        temp = temp->next;
+                        i++;
+                    }
+                }
+                free(args[0]);
+                free(args);
 
-        } else if (!strcmp(args[0], "kill")) {
-            // kill <jobID>: Send SIGTERM to the given job.
-
-        } else {
-            //this is where the stuff happens
-            //basically im putting working directory and other directories into path to see if it existss
-            struct stat buf;
-            char* path = malloc((strlen(pwd) + strlen(args[0]) + 11)*sizeof(char));
-            strcpy(path, pwd);
-            if (args[0][0] != '/') {
-                strcat(path, "/");
-                strcat(path, args[0]);
+            } else if (!strcmp(args[0], "kill")) {
+                // kill <jobID>: Send SIGTERM to the given job.
+                
+                free(args[0]);
+                free(args);
             } else {
-                strcpy(path, args[0]);
-            }
-            int exists = -1;
-            exists = stat(path, &buf);
-            if (exists) {
-                strcpy(path, "/usr/bin/");
-                strcat(path, args[0]);
+                //this is where the stuff happens
+                //basically im putting working directory and other directories into path to see if it existss
+                struct stat buf;
+                char* path = malloc((strlen(pwd) + strlen(args[0]) + 11)*sizeof(char));
+                strcpy(path, pwd);
+                if (args[0][0] != '/') {
+                    strcat(path, "/");
+                    strcat(path, args[0]);
+                } else {
+                    strcpy(path, args[0]);
+                }
+                int exists = -1;
                 exists = stat(path, &buf);
                 if (exists) {
-                    strcpy(path, "/bin/");
+                    strcpy(path, "/usr/bin/");
                     strcat(path, args[0]);
                     exists = stat(path, &buf);
+                    if (exists) {
+                        strcpy(path, "/bin/");
+                        strcat(path, args[0]);
+                        exists = stat(path, &buf);
+                    }
                 }
-            }
-            if (!exists) {
-                if (S_ISREG(buf.st_mode)) {
-                    if (!(tempPid = fork())) {
-                        execv(path, args);
-                        exit(status);
-                    } else {
+                if (!exists) {
+                    if (S_ISREG(buf.st_mode)) {
+                        if (!(tempPid = fork())) {
+                            execv(path, args);
+                            exit(status);
+                        } else {
 
-                        // HERE IS WHERE THE PARENT PROCESS BEGINS AFTER THE FORK
-                        // I THINK JOB STUFF GOES HERE
-                        // PUT JOB STUFF HERE
-                        // AND ALSO WHATEVER ELSE YOU WANT
-                        
-                        free(path);
-                        if (bg) {
-                            int stat;
-                            waitpid(tempPid, &stat, 0);
+                            // HERE IS WHERE THE PARENT PROCESS BEGINS AFTER THE FORK
+                            // I THINK JOB STUFF GOES HERE
+                            // PUT JOB STUFF HERE
+                            // AND ALSO WHATEVER ELSE YOU WANT
+                            
+                            job* newJob = malloc(sizeof(job));
+                            newJob->status = 1;
+                            newJob->command = args;                     // Need to find a way to concatenate all the strings
+                            newJob->next = NULL;
+                            newJob->pid = tempPid;
+                            newJob->commandLength = length;
+                            int i = 1;
+                            if (jobList->status == 0) {
+                                printf("[%d] %d %s %s\n", i, newJob->pid, getStatus(newJob->status), newJob->command[0]);
+                                newJob->next = jobList;
+                                jobList = newJob;
+                            }
+                            else {
+                                job* ptr = jobList;
+                                i++;
+                                while (ptr->next->status != 0){
+                                    ptr = ptr->next;
+                                    i++;
+                                }
+                                newJob->next = ptr->next;
+                                ptr->next = newJob;
+                                printf("[%d] %d %s %s\n", i, newJob->pid, getStatus(newJob->status), newJob->command[0]);
+                            }
+
+                            free(path);
+                            if (!bg) {
+                                int stat;
+                                waitpid(tempPid, &stat, 0);
+                            }
                         }
                     }
-                }
-                else {
-                    free(path);
-                    printf("%s: Is a directory\n", args[0]);
-                }
-            } else {
-                free(path);
-                int slash = 0;
-                for(int i = 0; i < strlen(args[0]); i++) {
-                    if (args[0][i] == '/') {
-                        slash = 1;
+                    else {
+                        free(path);
+                        printf("%s: Is a directory\n", args[0]);
                     }
-                }
-            
-                if (slash) {
-                    printf("%s: No such file or directory\n", args[0]);
                 } else {
-                    printf("%s: command not found\n", args[0]);
+                    free(path);
+                    int slash = 0;
+                    for(int i = 0; i < strlen(args[0]); i++) {
+                        if (args[0][i] == '/') {
+                            slash = 1;
+                        }
+                    }
+                
+                    if (slash) {
+                        printf("%s: No such file or directory\n", args[0]);
+                    } else {
+                        printf("%s: command not found\n", args[0]);
+                    }
                 }
             }
         }
-        for(int i = 0; i < length; i++) {
-            free(args[i]);
-        }
-        free(args);
-
-
-    //     } else {
-    //         char* createdProcess = malloc(2);
-    //         if (!strcmp(createdProcess, "0")) {
-    //             job* newJob = malloc(sizeof(job));
-    //             newJob->status = 1;
-    //             newJob->command = args;                     // Need to find a way to concatenate all the strings
-    //             newJob->next = NULL;
-    //             newJob->pid = tempPid;
-    //             i = 1;
-    //             if (jobList->status == 0) {
-    //                 printf("[%d] %d %s %s\n", i, newJob->pid, getStatus(newJob->status), newJob->command[0]);
-    //                 newJob->next = jobList;
-    //                 jobList = newJob;
-    //             }
-    //             else {
-    //                 job* ptr = jobList;
-    //                 i++;
-    //                 while(ptr->next->status != 0){
-    //                     ptr = ptr->next;
-    //                     i++;
-    //                 }
-    //                 newJob->next = ptr->next;
-    //                 ptr->next = newJob;
-    //                 printf("[%d] %d %s %s\n", i, newJob->pid, getStatus(newJob->status), newJob->command[0]);
-    //             }
-    //         }
-    //         free(createdProcess);
-    //     }
-
-    //     // Displays BG process Created
-    //     if (!bg) {
-    //         waitpid(tempPid, &status, 0);
-    //     }
-    //     else {
-    //         printf("[%d] %d\n", i, tempPid);
-    //     }
-            
-    //     i++;
-    //     // pid = realloc(pid, i * sizeof(pid_t));
-    //     if (feof(stdin)) {
-    //         executeChildProcess("exit", args, &pipefd);
-    //     }
-
-    //     for (int i = 0; i < length; i++) {
-    //         free(args[i]);
-    //     }
-    //     free(args);
-    // }    
-    // job* temp = jobList;
-    // while (jobList->status != 0) {
-    //     temp = temp->next;
-    //     free(jobList);
-    //     jobList = temp;
-    // }
-    // free(jobList);
-    // return 0;
+                
     }
+    for(int i = 0; i < length; i++) {
+        free(args[i]);
+    }
+    free(args); 
+    job* temp = jobList;
+    while (jobList->status != 0) {
+        temp = temp->next;
+        for (int i = 0; i < jobList->commandLength; i++) {
+            free(jobList->command[i]);
+        }
+        free(jobList->command);
+        free(jobList);
+        jobList = temp;
+    }
+    free(jobList);
+    return 0;
 }
